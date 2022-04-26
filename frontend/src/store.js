b268b30caed1e0c414a263309f8890f3e1ad333f
Mayank91771 <mayankm91771@gmail.com>
2022-04-26 16:24:47 -0400

    Updated cart count code

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,14 +11,26 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CART_ADD_ITEM":
-      // keep all previous values in the state and inside cart obj add a item only in the cartItem
-      return {
-        ...state, //keep all previous values in the state
-        cart: {
-          ...state.cart, // keep all previous values in cart in state and only update cartItems in next line
-          cartItems: [...state.cart.cartItems, action.payload], // [keep all the previous cart items, add the new item]
-        },
-      };
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item._id === newItem._id
+      );
+      const cartItems = existItem //we cheeck if we already have existItem else we add it
+        ? state.cart.cartItems.map(
+            (item) => (item._id === existItem._id ? newItem : item) // update current item with newItem, i.e., update qty : else keep the current item
+          )
+        : [...state.cart.cartItems, newItem];
+      return { ...state, cart: { ...state.cart, cartItems } };
+
+    // return {
+    //   // keep all previous values in the state and inside cart obj add a item only in the cartItem
+    //   ...state, //keep all previous values in the state
+    //   cart: {
+    //     ...state.cart, // keep all previous values in cart in state and only update cartItems in next line
+    //     cartItems: [...state.cart.cartItems, action.payload], // [keep all the previous cart items, add the new item]
+    //   },
+    // };
+
     default:
       return state;
   }
